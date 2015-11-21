a930c03ab23f57b60d61659e169304ead682f450
npham014 <npham014@ucr.edu>
2015-11-21 21:28:00 +0900

    allfinished

--- problems/p4/palindrome.h
+++ problems/p4/palindrome.h
@@ -1,11 +1,18 @@
 
 #include <string>
-
+#include <cctype>
 
 
 static bool isPalindrome(std::string s)
 {
 	bool isPalin = false;
+	
+	for(unsigned j = 0; j < (s.size() - 1); ++j){
+		if(!(isalpha(s.at(j)))){
+			s.erase(j,1);
+		}
+	}
+	
 	
 	for(unsigned i = 0; i < (s.size() / 2); ++i){
 		if(s.at(i) == s.at((s.size() - 1) - i)){

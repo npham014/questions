284a1d7ade9ddfcddf0e2c7d76adfc7c0aa69718
npham014 <npham014@ucr.edu>
2015-11-21 19:38:13 +0900

    Modified palindrome

--- problems/p4/palindrome.h
+++ problems/p4/palindrome.h
@@ -1,7 +1,16 @@
 #include <string>
 static bool isPalindrome(std::string s)
 {
-	(void)s;
-	/* code goes here */
-	return false;
+	bool isPalin = false;
+	
+	for(int i = 0; i < userString.size() / 2; ++i){
+		if(userString.at(i) == userString.at(userString.size() - i)){
+			isPalin = true;
+		}
+		else{
+			isPalin = false;
+		}
+
+	}
+	return isPalin;
 }
